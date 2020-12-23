acaa16a4d6762b8ba7828d01077e12fff89923c0
Robert H. Anstett <rob@southportsolutions.com>
2020-12-22 16:31:41 -0800

    added EmailResult

---
+++ Southport.Messaging.Email.Core/Result/EmailResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using Southport.Messaging.Email.Core.Recipient;
+
+namespace Southport.Messaging.Email.Core.Result
+{
+    public class EmailResult : IEmailResult
+    {
+        public EmailResult(IEmailRecipient emailRecipient, HttpResponseMessage responseMessage)
+        {
+            EmailRecipient = emailRecipient;
+            ResponseMessage = responseMessage;
+        }
+        public IEmailRecipient EmailRecipient { get; set; }
+        public HttpResponseMessage ResponseMessage { get; set; }
+    }
+}
