6caf507cc251edb9242a5ed4ddf9a50513bf7b96
Robert H. Anstett <rob@southportsolutions.com>
2020-12-28 15:32:22 -0600

    release 1.1.0-beta3.

--- Southport.Messaging.Email.Core/Recipient/EmailRecipient.cs
+++ Southport.Messaging.Email.Core/Recipient/EmailRecipient.cs
@@ -9,6 +9,7 @@ namespace Southport.Messaging.Email.Core.Recipient
         {
             Substitutions = new Dictionary<string, object>();
             Attachments = new List<IEmailAttachment>();
+            CustomArguments = new Dictionary<string, string>();
         }
         public EmailRecipient(string address) : this()
         {
@@ -22,14 +23,22 @@ namespace Southport.Messaging.Email.Core.Recipient
         {
             Substitutions = substitutions;
         }
+        public EmailRecipient(string name, string address, Dictionary<string, object> substitutions, Dictionary<string, string> customArguments) : this(name, address, substitutions)
+        {
+            CustomArguments = customArguments;
+        }
         public EmailRecipient(string address, Dictionary<string, object> substitutions) : this(address)
         {
             Substitutions = substitutions;
         }
+        public EmailRecipient(string address, Dictionary<string, object> substitutions, Dictionary<string, string> customArguments) : this(address, substitutions)
+        {
+            CustomArguments = customArguments;
+        }
 
         public EmailAddress EmailAddress { get; set; }
-        public Dictionary<string, object> Substitutions { get; set; }
-        public List<IEmailAttachment> Attachments { get; set; }
-
+        public Dictionary<string, object> Substitutions { get; }
+        public List<IEmailAttachment> Attachments { get; }
+        public Dictionary<string, string> CustomArguments { get; }
     }
 }
--- Southport.Messaging.Email.Core/Recipient/IEmailRecipient.cs
+++ Southport.Messaging.Email.Core/Recipient/IEmailRecipient.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using Southport.Messaging.Email.Core.EmailAttachments;
 
 namespace Southport.Messaging.Email.Core.Recipient
 {
     public interface IEmailRecipient
     {
-        EmailAddress EmailAddress { get; set; }
-        Dictionary<string, object> Substitutions { get; set; }
-        List<IEmailAttachment> Attachments { get; set; }
+        EmailAddress EmailAddress { get; }
+        Dictionary<string, object> Substitutions { get; }
+        List<IEmailAttachment> Attachments { get; }
+        Dictionary<string, string> CustomArguments { get; }
     }
 }
