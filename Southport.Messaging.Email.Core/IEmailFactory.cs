57f011d9ecf207b146269fce21165aeeee14fa9d
Robert H. Anstett <rob@southportsolutions.com>
2021-07-27 21:27:36 -0500

    documented the iemailfactory

--- Southport.Messaging.Email.Core/IEmailFactory.cs
+++ Southport.Messaging.Email.Core/IEmailFactory.cs
@@ -1,8 +1,27 @@
-﻿namespace Southport.Messaging.Email.Core
+﻿// ***********************************************************************
+// Assembly         : Southport.Messaging.Email.Core
+// Author           : RobertHAnstett
+// Created          : 07-27-2021
+//
+// Last Modified By : RobertHAnstett
+// Last Modified On : 07-27-2021
+// ***********************************************************************
+// <copyright file="IEmailFactory.cs" company="Southport Solutions, LLC">
+//     2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace Southport.Messaging.Email.Core
 {
+    /// <summary>
+    /// Interface IEmailMessageFactory
+    /// </summary>
     public interface IEmailMessageFactory
     {
+        /// <summary>
+        /// Creates this instance.
+        /// </summary>
+        /// <returns>IEmailMessageCore.</returns>
         IEmailMessageCore Create();
-        
     }
 }
